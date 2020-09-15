ba73c35a09539ba8fc51d9dd6106eb70de07fb96
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-15 05:06:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,7 @@ def sort_array_char_count(strings)
 end
 
 def swap_elements(swap)  #shift out 1st element, swap 2 elements, unshift back to front
-  swap.swap!(1,2)
+  swap.swap(1,2)
 end
 
 def reverse_array(numbers)
