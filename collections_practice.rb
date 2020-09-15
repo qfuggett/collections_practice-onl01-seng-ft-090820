bf76b0605bf19e6240f596b123a8ecae5ead6b76
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-15 03:44:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(numbers)
+  numbers.sort!
+end
 
+def sort_array_desc(numbers)
+  numbers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(swap)
+  swap.sort do |a, b|
+    temp = a
+    a = b
+    b = temp
+    swapped = a, b
+  end
+  return swapped
+end
+
+def reverse_array(numbers)
+  numbers.reverse
+end
+
+def kesha_maker(names)
+  kesha_name = [ ]
+  names.each do |individual_name|
+    individual_name.slice!(2)
+    kesha_name = individual_name.insert 2, "$"
+  end
+end
+
+def find_a(words)
+  words.select do |string|
+    string.start_with?("a")       #returns true for elements passed starting with a
+  end
+end
+
+def sum_array(numbers)
+  numbers.inject do |numbers, x|      #passes each element and accumulates sequentially
+    numbers + x                     
+  end
+end
+
+def add_s(words)
+  remove = words.reject! {|w| w = words[1]}
+  words.each do |element|
+    element << "s"
+    element << remove
+  end
+end
